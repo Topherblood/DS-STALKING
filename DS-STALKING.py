526de67dad6d5b822bcc3e87cb8aebd2e13e7daa
Topherblood <162853344+Topherblood@users.noreply.github.com>
2024-12-12 23:50:52 -0500

    Update DS-STALKING.py

--- DS-STALKING.py
+++ DS-STALKING.py
@@ -42,16 +42,16 @@ def home():
     # Cette fonction rend le fichier camera.html lorsqu'on visite la page d'accueil
     return render_template("camera.html")
 
-@app.route("/stream_link", methods=["GET"])
+@app.route("/stream_link", methods=["POST"])
 def stream_link():
-    # Génère un lien contenant l'adresse IP et le port pour accéder au flux vidéo
+    # Génère un lien contenant l'adresse IP et le port
     client_ip = request.remote_addr
-    port = 4444  # Port du serveur Flask (modifiez-le si nécessaire)
-    stream_url = f"http://{client_ip}:{port}/video_stream"  # URL du flux vidéo
+    port = 5000  # Utilisez le port 5000 ou tout autre port disponible
+    stream_url = f"http://{client_ip}:{port}/stream"
     return jsonify({"stream_url": stream_url})
 
 if __name__ == "__main__":
     # Exécuter le serveur Flask
     local_ip = get_local_ip()
-    print(f"Serveur en cours d'exécution : http://{local_ip}:4444")
-    app.run(host="0.0.0.0", port=4444)  # Démarrer le serveur sur le port 4444
+    print(f"Serveur en cours d'exécution : http://{local_ip}:5000")  # Modifié le port ici
+    app.run(host="0.0.0.0", port=5000)  # Démarrer le serveur sur le port 5000
